cde5524797f9e31ba14189a22cdb81163a273c3c
Cecília <130860320+mimistudies90@users.noreply.github.com>
2025-06-06 20:23:19 -0300

    Create process_login.php

---
+++ public/process_login.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require_once __DIR__ . '/../models/UsuarioDAO.php';
+
+require_once __DIR__ .  '/../utils/Sanitizacao.php';
+
+// Sanitiza as entradas
+$email = Sanitizacao::sanitizar($_POST['email']);
+$senha = Sanitizacao::sanitizar($_POST['senha']);
+
+// Valida o login
+$usuarioDAO = new UsuarioDAO();
+$usuario = $usuarioDAO->validarLogin($email, $senha);
+
+if ($usuario) {
+    $_SESSION['logado'] = true;
+    $_SESSION['usuario_id'] = $usuario->getId();
+    echo "Login bem-sucedido!";
+} else {
+    echo "Email ou senha incorretos.";
+}
+?>
